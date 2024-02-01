5eb27e7d5f10341d8aac942da5fdf99ccc692628
Mikołaj Wyka <mikolaj.wyka21@gmail.com>
2024-02-01 22:53:03 +0100

    Update week 2

--- week_2/list_class.py
+++ week_2/list_class.py
@@ -7,6 +7,15 @@ class Node:
         return str(self.data)
 
 
+class DoubleNode(Node):
+    def __init__(self, data, next=None, prev=None):
+        super().__init__(data, next)
+        self.prev = prev
+
+    def __str__(self):
+        return str(self.data)
+
+
 class List:
     def __init__(self, head=None):
         self.head = head
@@ -49,12 +58,14 @@ class List:
                 last.next = None
 
     def print(self):
-        pointer = self.head
+        if not self.is_empty:
+            print("Single List: ", end="")
+            pointer = self.head
 
-        while pointer:
+            while pointer.next:
+                print(pointer, end=" -> ")
+                pointer = pointer.next
             print(pointer)
-            pointer = pointer.next
-        pass
 
     @property
     def is_empty(self):
@@ -75,3 +86,62 @@ class List:
     @property
     def get(self):
         return self.head.data
+
+
+class DoubleList(List):
+    def __init__(self, head=None, tail=None):
+        super().__init__(head)
+        self.tail = tail
+
+    def destroy(self):
+        pointer = self.tail
+
+        while pointer:
+            last = pointer
+            pointer.prev = None
+            pointer = last.prev
+
+        self.head = None
+
+    def add(self, other):
+        if self.is_empty:
+            new_node = DoubleNode(other)
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node = DoubleNode(other, self.head)
+            self.head.prev = new_node
+            self.head = new_node
+
+    def append(self, other):
+        if self.is_empty:
+            new_node = DoubleNode(other)
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node = DoubleNode(other, prev=self.tail)
+            self.tail.next = new_node
+            self.tail = new_node
+
+    def remove(self):
+        if not self.is_empty:
+            self.head.prev = None
+            self.head = self.head.next
+
+    def remove_end(self):
+        if not self.is_empty:
+            if self.length == 1:
+                self.destroy()
+            else:
+                self.tail = self.tail.prev
+                self.tail.next = None
+
+    def print(self):
+        if not self.is_empty:
+            print("Double List: ", end="")
+            pointer = self.head
+
+            while pointer.next:
+                print(pointer, end=" <-> ")
+                pointer = pointer.next
+            print(pointer)
--- week_2/main.py
+++ week_2/main.py
@@ -1,4 +1,53 @@
-import list_class
+from list_class import *
+
+
+def single_list_process(data):
+    single_list = List()
+
+    for i in range(3):
+        single_list.append(data[i])
+
+    for elem in data:
+        single_list.add(elem)
+
+    single_list.print()
+    print('Number of elements: ', single_list.length)
+    single_list.remove()
+    print('Head: ', single_list.head)
+    single_list.remove_end()
+    single_list.print()
+    single_list.destroy()
+    print('Is Single List empty?: ', single_list.is_empty)
+    single_list.remove()
+    single_list.append(data[0])
+    single_list.remove_end()
+    print('Is Single List empty?: ', single_list.is_empty)
+    single_list.print()
+
+
+def double_list_process(data):
+    double_list = DoubleList()
+
+    for i in range(3):
+        double_list.append(data[i])
+
+    for elem in data:
+        double_list.add(elem)
+
+    double_list.print()
+    print('Number of elements: ', double_list.length)
+    double_list.remove()
+    print('Head: ', double_list.head)
+    double_list.remove_end()
+    double_list.print()
+    double_list.destroy()
+    print('Is Double List empty?: ', double_list.is_empty)
+    double_list.remove()
+    double_list.append(data[0])
+    double_list.remove_end()
+    print('Is Double List empty?: ', double_list.is_empty)
+    double_list.print()
+
 
 if __name__ == '__main__':
     universities = [('AGH', 'Kraków', 1919),
@@ -7,25 +56,8 @@ if __name__ == '__main__':
                     ('UW', 'Warszawa', 1915),
                     ('UP', 'Poznań', 1919),
                     ('PG', 'Gdańsk', 1945)]
+    print("1. Single Linked List")
+    single_list_process(universities)
 
-    list = list_class.List()
-
-    for i in range(3):
-        list.append(universities[i])
-
-    for elem in universities:
-        list.add(elem)
-
-    list.print()
-    print('\n', list.length)
-    list.remove()
-    print('\n', list.head)
-    list.remove_end()
-    list.print()
-    list.destroy()
-    print('\n', list.is_empty)
-    list.remove()
-    list.append(universities[0])
-    list.remove_end()
-    print('\n', list.is_empty)
-    list.print()
+    print("\n2. Doubled Linked List")
+    double_list_process(universities)
